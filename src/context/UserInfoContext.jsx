8ce53324055c55beb7ec4478ca4cfa660e4a41c6
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2024-01-07 23:44:35 +0100

    Update user info context

--- src/context/UserInfoContext.jsx
+++ src/context/UserInfoContext.jsx
@@ -6,8 +6,16 @@ UserContext.displayName = "UserContext";
 
 const UserInfoContext = ({children}) => {
 
+    // Some dummy user info
     const userInfo = {
         name: "Andrii",
+        age: 30,
+        email: "andrii@example.com",
+        address: {
+            street: "123 Main St",
+            city: "Kyiv",
+            country: "Ukraine"
+        },
         permissions: ["admin"]
     }
 
