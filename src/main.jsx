e803391f8576ca982af7ce836b19dfeeaa764874
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2024-02-06 04:28:36 +0100

    Add Redux, create auth reducer

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import {BrowserRouter} from "react-router-dom";
 import UserInfoContext from "./context/UserInfoContext.jsx";
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <UserInfoContext>
         <BrowserRouter>
-            <App/>
+            <Provider store={store}>
+                <App/>
+            </Provider>
         </BrowserRouter>
     </UserInfoContext>
 );
